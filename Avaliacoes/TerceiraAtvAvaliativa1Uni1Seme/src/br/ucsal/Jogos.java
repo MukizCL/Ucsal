12e5050ea55c58087ec36cbfb2c190d155150022
Diegofdev <dfsantosba@gmail.com>
2020-05-27 20:07:58 -0300

    Update Jogos.java

--- Avaliacoes/TerceiraAtvAvaliativa1Uni1Seme/src/br/ucsal/Jogos.java
+++ Avaliacoes/TerceiraAtvAvaliativa1Uni1Seme/src/br/ucsal/Jogos.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class Jogos {
 
 	public static void main(String[] args) {
-		Scanner imput = new Scanner(System.in);
+		/*Scanner imput = new Scanner(System.in);
 		String[] usuario = new String[2];
 		imprimir("Insira o nome do usuario 1: \n");
 		usuario[0] = imput.next();
@@ -46,11 +46,87 @@ public class Jogos {
 		} else if( menu == 4) {
 			imprimir("Jogo encerrado!");
 			System.exit(0);
-		}
+		} */
+		
+		cadastroUsuario("", 0);
+		
 
 
 
 	}
+	public static void cadastroUsuario(String user, int op) {
+		Scanner imput = new Scanner(System.in);
+		String[] usuario = new String[2];
+		usuario[0] = user;
+		if(usuario[0].equals("")) {
+		imprimir("Insira o nome do usuario 1: \n");
+		usuario[0] = imput.next();
+		validaUsuario(0);
+		imprimir ("Usuario1: " + usuario[0] + " - Validado com sucesso" + "\n");
+		imprimir("\n ==========================="
+				+ "\n Seja bem vindo: " + usuario[0]);
+		usuario[1] = "";
+		menu(usuario, op);
+		} else {
+			imprimir("Insira o nome do usuario 2\n");
+			usuario[1] = imput.next();
+			validaUsuario(111);
+			imprimir ("Usuario2: " + usuario[1] + " - Validado com sucesso" + "\n"); 
+			imprimir("\n ==========================="
+					+ "\n Sejam bem vindos: " + usuario[0] + " e " + usuario[1]);
+			menu(usuario, op);
+		}
+	}
+	
+	//MÉTODO QUE IMPRIME O MENU:
+		public static void menu (String[] usuario, int op) {
+			String user1 = usuario[0];
+			String user2 = usuario[1];
+			Scanner imput = new Scanner(System.in);
+			int menu = op;
+			if(menu == 0) {
+			imprimir("\n ==========================="
+					+ "\n Escolha qual jogo deseja jogar:"
+					+ "\n [1] Jogo da forca"
+					+ "\n [2] Batalha naval"
+					+ "\n [3] Campo minado"
+					+ "\n [4] Sair\n");
+			menu = imput.nextInt();
+			}
+			
+			switch(menu) {
+			case 1:
+				if(user2.equals("")) {
+					imprimir("Este jogo requer 2 usuários\n");
+					cadastroUsuario(usuario[0], menu);
+					} else {
+					
+					imprimir("Iniciando Jogo da forca...");
+					}
+					break;
+			case 2:
+				if(user2.equals("")) {
+					imprimir("Este jogo requer 2 usuários\n");
+					cadastroUsuario(usuario[0], menu);
+					} else {
+					
+					imprimir("Iniciando Batalha naval...");
+					}
+					break;
+			case 3: 
+				imprimir("\nIniciando Campo minado");
+				break;
+			case 4:
+				imprimir("Jogo encerrado!");
+				System.exit(0);
+				break;
+			default:
+				imprimir("Você digitou uma opção inválida, Tente novamente: \n");
+				
+				break;
+			}
+		}
+	
 	// MÉTODO PARA VALIDAÇÃO DA SENHA DO USUARIO:
 	public static void validaUsuario (int pass) {
 		Scanner imput = new Scanner(System.in);
@@ -76,15 +152,7 @@ public class Jogos {
 		}
 
 	}
-	//MÉTODO QUE IMPRIME O MENU:
-	public static void menu () {
-		imprimir("\n ==========================="
-				+ "\n Escolha qual jogo deseja jogar:"
-				+ "\n [1] Jogo da forca"
-				+ "\n [2] Batalha naval"
-				+ "\n [3] Campo minado"
-				+ "\n [4] Sair\n");
-	}
+	
 
 
 
