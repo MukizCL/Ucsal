e74beab549875dcd0b20b6545dce62234571a973
Diegofdev <dfsantosba@gmail.com>
2020-05-29 22:36:10 -0300

    Update Jogos.java
    Jogo da forca finalizado para 2

--- Avaliacoes/TerceiraAtvAvaliativa1Uni1Seme/src/br/ucsal/Jogos.java
+++ Avaliacoes/TerceiraAtvAvaliativa1Uni1Seme/src/br/ucsal/Jogos.java
@@ -1,5 +1,6 @@
 package br.ucsal;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 
@@ -9,7 +10,7 @@ public class Jogos {
 	public static void main(String[] args) {
 		bemVindo();
 		cadastroUsuario("", 0);
-		
+
 
 
 
@@ -20,21 +21,21 @@ public class Jogos {
 				+ "\nAntes de irmos para o menu, Faça o login"
 				+ "\n==========================================\n");
 	}
-	
+
 	//MÉTODO QUE CADASTRA OS USUÁRIOS:
 	public static void cadastroUsuario(String user, int op) {
 		Scanner imput = new Scanner(System.in);
 		String[] usuario = new String[2];
 		usuario[0] = user;
 		if(usuario[0].equals("")) {
-		imprimir("Insira o nome do usuario 1: \n");
-		usuario[0] = imput.next();
-		validaUsuario(0);
-		imprimir ("Usuario1: " + usuario[0] + " - Validado com sucesso" + "\n");
-		imprimir("\n ==========================="
-				+ "\n Seja bem vindo: " + usuario[0]);
-		usuario[1] = "";
-		menu(usuario, op);
+			imprimir("Insira o nome do usuario 1: \n");
+			usuario[0] = imput.next();
+			validaUsuario(0);
+			imprimir ("Usuario1: " + usuario[0] + " - Validado com sucesso" + "\n");
+			imprimir("\n ==========================="
+					+ "\n Seja bem vindo: " + usuario[0]);
+			usuario[1] = "";
+			menu(usuario, op);
 		} else {
 			imprimir("Insira o nome do usuario 2\n");
 			usuario[1] = imput.next();
@@ -45,14 +46,14 @@ public class Jogos {
 			menu(usuario, op);
 		}
 	}
-	
+
 	//MÉTODO QUE IMPRIME O MENU:
-		public static void menu (String[] usuario, int op) {
-			String user1 = usuario[0];
-			String user2 = usuario[1];
-			Scanner imput = new Scanner(System.in);
-			int menu = op;
-			if(menu == 0) {
+	public static void menu (String[] usuario, int op) {
+		String user1 = usuario[0];
+		String user2 = usuario[1];
+		Scanner imput = new Scanner(System.in);
+		int menu = op;
+		if(menu == 0) {
 			imprimir("\n ==========================="
 					+ "\n Escolha qual jogo deseja jogar:"
 					+ "\n [1] Jogo da forca"
@@ -60,50 +61,51 @@ public class Jogos {
 					+ "\n [3] Campo minado"
 					+ "\n [4] Sair\n");
 			menu = imput.nextInt();
-			}
-			
-			switch(menu) {
-			case 1:
-				if(user2.equals("")) {
-					imprimir("Este jogo requer 2 usuários\n");
-					cadastroUsuario(usuario[0], menu);
-					} else {
-					
-					imprimir("\n ==========================="
-							+ "\nIniciando Jogo da forca...");
-					}
-					break;
-			case 2:
-				if(user2.equals("")) {
-					imprimir("Este jogo requer 2 usuários\n");
-					cadastroUsuario(usuario[0], menu);
-					} else {
-					
-					imprimir("\n ==========================="
-							+ "\nIniciando Batalha naval...");
-					}
-					break;
-			case 3: 
-				imprimir("\nIniciando Campo minado");
-				break;
-			case 4:
-				imprimir("Jogo encerrado!");
-				System.exit(0);
-				break;
-			default:
-				imprimir("Você digitou uma opção inválida, Tente novamente: \n");
-				
-				break;
-			}
 		}
-	
+
+		switch(menu) {
+		case 1:
+			if(user2.equals("")) {
+				imprimir("Este jogo requer 2 usuários\n");
+				cadastroUsuario(usuario[0], menu);
+			} else {
+
+				imprimir("\n ==========================="
+						+ "\nIniciando Jogo da forca...\n\n");
+				inicioForca(user1, user2);
+			}
+			break;
+		case 2:
+			if(user2.equals("")) {
+				imprimir("Este jogo requer 2 usuários\n");
+				cadastroUsuario(usuario[0], menu);
+			} else {
+
+				imprimir("\n ==========================="
+						+ "\nIniciando Batalha naval...");
+			}
+			break;
+		case 3: 
+			imprimir("\nIniciando Campo minado");
+			break;
+		case 4:
+			imprimir("Jogo encerrado!");
+			System.exit(0);
+			break;
+		default:
+			imprimir("Você digitou uma opção inválida, Tente novamente: \n");
+
+			break;
+		}
+	}
+
 	// MÉTODO PARA VALIDAÇÃO DA SENHA DO USUARIO:
 	public static void validaUsuario (int pass) {
 		Scanner imput = new Scanner(System.in);
 		int[] senha = new int[2];
 		senha[0] = pass;
-		
-		
+
+
 		if(senha[0] != 111) {
 			imprimir("Insira a senha do Usuario 1"
 					+ "                     'A senha para o usuario 1 é: 111' \n");
@@ -125,6 +127,202 @@ public class Jogos {
 
 	}
 	
+    //INICIO JOGO DA FORCA
+	
+
+	
+	
+	public static int resultForca = 0;
+	public static void inicioForca(String user1, String user2) {
+		int scorep1 = 0, scorep2 = 0;
+		int forcagame = 1;
+		
+		imprimir("Bem vindo ao jogo da forca. "
+				+ "\nVocê terá 6 tentativas para adivinhar a palavra dita pelo seu oponente\n"
+				+ "\nquem obtiver menos erros ganha! \n");
+		for (int i = 0; i <= 1; i++) {
+			
+		
+		if(forcagame == 1) {
+			
+			forca(user1, user2);
+			scorep1 = resultForca;
+		}
+		if(forcagame == 2) {
+			
+			forca(user2, user1);
+			scorep2 = resultForca;
+		}
+		}
+		if(scorep1 < scorep2) {
+			imprimir("O Jogador " + user1 + " ganhou de " + user2 + " com apenas " + scorep1 + " erros, contra " + scorep2 + " do jogador " + user2 );
+		} else if (scorep1 > scorep2) {
+			imprimir("O Jogador " + user2 + " ganhou de " + user1 + " com apenas " + scorep2 + " erros, contra " + scorep1 + " do jogador " + user1 );
+		} else if (scorep1 == scorep2) {
+			imprimir("O jogo empatou");
+		}
+		
+	}
+	
+	public static void forca(String user1, String user2) {
+		Scanner imput = new Scanner(System.in);
+		imprimir("\nAgora é a vez de " + user1 + " digitar uma palavra para " + user2 + " adivinhar");
+		imprimir("\nDigite a palavra: ");
+		String palavra = imput.next().toUpperCase();
+		//char[] palavraSeparada = null;
+		//palavraSeparada = palavra.toCharArray();
+		//String[] palavraSeparada1 = new String[palavra.length()];
+		
+		imprimir("\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+		
+		forcaEnforcado(0, palavra);
+		resultForca = 0;
+		imprimir("\n");
+		String[] palavraTemp = new String[palavra.length()];
+		String[] palavraTemp2 = palavra.split("");
+		for (int i = 0; i < palavra.length(); i++) {
+			imprimir("-");
+			palavraTemp[i] = "-";
+			
+		}
+		
+		imprimir("\n");
+		
+		int sair = 0;
+		for (int i = 0; sair < 6; i++) {
+			imprimir("\n" + user2 + " Digite uma letra: \n");
+			String letra = imput.next().toUpperCase();
+			
+			
+			
+			for (int j = 0; j < palavra.length(); j++) {
+				
+				if(letra.charAt(0) == palavra.charAt(j)) {
+					imprimir(letra);
+					palavraTemp[j] = letra;
+				} else { 
+					if(palavraTemp[j] != "-") {
+						imprimir(palavraTemp[j]);
+					} else {
+					imprimir("-");
+					
+					
+					
+					}
+				}
+				if(Arrays.equals(palavraTemp, palavraTemp2)) {
+					sair = 6;
+					
+				}
+			}
+			for (int j = 0, a = 0, b = 0; j < palavra.length(); j++) {
+				if(letra.charAt(0) != palavra.charAt(j)) {
+					a++;
+				} else { 
+					b++;
+				}
+				if(a == palavra.length()) {
+					resultForca++;
+					sair++;
+					forcaEnforcado(resultForca, palavra);
+				} else if (j == palavra.length()-1 && b>0 ) { 
+					imprimir("\nParabens, você acertou a letra\n");
+				}
+			}
+		}
+	}
+	
+		
+
+	public static void forcaEnforcado(int contador, String palavra) {
+		if (contador == 0) {
+			System.out.println("");
+			System.out.println("   ____________");
+			System.out.println("   |           |");
+			System.out.println("   |           ");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("   | ");
+			System.out.println("___|___");
+		}
+		if (contador == 1) {
+			System.out.println("\nVocê errou, Tente novamente");
+			System.out.println("   ____________");
+			System.out.println("   |           |");
+			System.out.println("   |           O");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("   | ");
+			System.out.println("___|___");
+		}
+		if (contador == 2) {
+			System.out.println("\nVocê errou, Tente novamente");
+			System.out.println("   ____________");
+			System.out.println("   |           |");
+			System.out.println("   |           O");
+			System.out.println("   |           |");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("   | ");
+			System.out.println("___|___");
+		}
+		if (contador == 3) {
+			System.out.println("\nVocê errou, Tente novamente");
+			System.out.println("   ____________");
+			System.out.println("   |           |");
+			System.out.println("   |           O");
+			System.out.println("   |          /|");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("   | ");
+			System.out.println("___|___");
+		}
+		if (contador == 4) {
+			System.out.println("\nVocê errou, Tente novamente");
+			System.out.println("   ____________");
+			System.out.println("   |           |");
+			System.out.println("   |           O");
+			System.out.println("   |          /|\\");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("   | ");
+			System.out.println("___|___");
+		}
+		if (contador == 5) {
+			System.out.println("\nVocê errou, Tente novamente");
+			System.out.println("   ____________");
+			System.out.println("   |           |");
+			System.out.println("   |           O");
+			System.out.println("   |          /|\\");
+			System.out.println("   |          /   ");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("   | ");
+			System.out.println("___|___");
+		}
+		if (contador == 6) {
+			System.out.println("\nVocê errou pela ultima vez");
+			System.out.println("   ____________");
+			System.out.println("   |           |");
+			System.out.println("   |           O");
+			System.out.println("   |          /|\\");
+			System.out.println("   |          / \\");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("   | ");
+			System.out.println("___|___");
+			System.out.println("Fim de jogo, a palavra era " + palavra);
+		}
+
+	}
+
 
 
 
